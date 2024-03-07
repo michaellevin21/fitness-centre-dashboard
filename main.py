8759cf376a3c949af94800d492684fb3744021bf
Michael Levin <112990274+michaellevin21@users.noreply.github.com>
2024-03-07 01:01:02 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -18,15 +18,35 @@ if __name__ == '__main__':
     conn = connect(config)
     db = conn.cursor()
 
+
+def getAllStudents():
+    db.execute('select * from Students')
+    for result in db:
+        print(result)
+
+def addStudent(first_name, last_name, email, enrollment_date):
+    db.execute(f"insert into Students (first_name, last_name, email, enrollment_date) values ('{first_name}', '{last_name}', '{email}', '{enrollment_date}')")
+    conn.commit()
+    print("Succesfully added student")
+
+
+def updateStudentEmail(student_id, new_email):
+    db.execute(f"update Students set email = '{new_email}' where student_id = '{student_id}'")
+    conn.commit()
+    print("Succesfully changed email")
+
+def deleteStudent(student_id):
+    db.execute(f"delete from Students where student_id = '{student_id}'")
+    conn.commit()
+    print("Succesfully deleted student")
+
 print("1. Get all students")
 print("2. Add a student")
 print("3. Update a student's email")
 print("4. Delete a student")
 num = int(input())
 if(num == 1):
-    db.execute('select * from Students')
-    for result in db:
-        print(result)
+    getAllStudents()
 
 elif(num == 2):
     fname = input("Please type in the student's first name: ")
@@ -34,17 +54,16 @@ elif(num == 2):
     email = input("Please type in the students email: ")
     day = datetime.date(int(input("Please type in the year of enrollment: ")),int(input("Please type in the month (1-12) of enrollment: ")),int(input("Please type in the day(1-31) of enrollment:")))  
     day = day.strftime('%Y-%m-%d')
-    db.execute(f"insert into Students (first_name, last_name, email, enrollment_date) values ('{fname}', '{lname}', '{email}', '{day}')")
-    conn.commit()
-    print("Succesfully added student")
-
+    addStudent(fname,lname,email,day)
+    
 elif(num == 3):
-    id = int(input("Please type in the id of the student you would like to change the email of:"))
-    db.execute('select student_id from Students')
-    if(id not in db):
-        print("There is no student with this id")
-    else:
-        
+    id = int(input("Please type in the id of the student you would like to change the email of: "))
+    email = input("Please type in the new email you want this student to have: ")
+    updateStudentEmail(id,email)
+
+elif(num == 4):
+    deleteStudent(int(input("Please type in the id of the student you would like to delete: ")))
+    
 
   
 
