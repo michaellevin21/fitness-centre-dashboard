6e48151897af71c6ab88e90abbeeb008800d9d1a
Michael Levin <112990274+michaellevin21@users.noreply.github.com>
2024-03-08 01:01:18 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -18,6 +18,11 @@ if __name__ == '__main__':
     conn = connect(config)
     db = conn.cursor()
 
+db.execute("DROP TABLE IF EXISTS Students")
+db.execute("CREATE TABLE students (student_id SERIAL PRIMARY KEY,first_name TEXT NOT NULL,last_name TEXT NOT NULL,email TEXT NOT NULL UNIQUE,enrollment_date DATE)")
+db.execute("INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES('John', 'Doe', 'john.doe@example.com', '2023-09-01'),('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02');")
+conn.commit()
+print("Succesfully created and filled database")
 
 def getAllStudents():
     db.execute('select * from Students')
